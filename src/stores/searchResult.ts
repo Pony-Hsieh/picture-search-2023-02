3efb70eeb8c376eb95e0d78025b0a4f4040b58e2
Pony Hsieh <lbjwin@gmail.com>
2023-02-14 11:13:51 +0800

    feat: 更新搜尋頁面相關功能

--- src/stores/searchResult.ts
+++ src/stores/searchResult.ts
@@ -8,55 +8,63 @@ export const useSearchResultStore = defineStore("searchResult", {
   state: () => ({
     // 目前存資料的搜尋關鍵字
     searchKeyword: "",
-    // AJAX load 回來的所有資料 (這個也許不用存，因為存進 showPhotos 之後，這個東西就用不到了)
-    allResultdata: {} as IPhotoSearchResult,
     // 目前展示的內容
     showPhotos: [] as Array<ISinglePhoto>,
     // 是否有上一頁
     havePrevPage: false,
     // 是否有下一頁
     haveNextPage: false,
+    // 資料庫中是否有圖片
+    havePhotosInDatabase: false,
   }),
   actions: {
+    /**
+     * 更新當前搜尋關鍵字
+     * 可以無論如何都存，但是如果沒有圖片可以顯示的話，再告知使用者目前沒有照片可供顯示
+     */
+    updateSearchKeyword(q: string) {
+      this.searchKeyword = q;
+    },
+    /**
+     * 判斷 load 回來的資料
+     */
+    judgeSearchResult(photoSearchRes: IPhotoSearchResult) {
+      // const searchResult = useSearchResultStore();
+      // 是否有上一頁
+      if (photoSearchRes.prev_page) {
+        this.havePrevPage = true;
+      }
+      // 是否有下一頁
+      if (photoSearchRes.next_page) {
+        this.haveNextPage = true;
+      }
+      // 資料庫中是否有該關鍵字的圖片
+      if (photoSearchRes.page === 1 && photoSearchRes.photos.length === 0) {
+        this.havePhotosInDatabase = false;
+      } else {
+        this.havePhotosInDatabase = true;
+      }
+    },
     /**
      * 清除搜尋結果
      * 不過如果有 `store.$reset()` 這個方法，不確定這個 function 還有沒有需要留
      */
     clearSearchResult() {
       // function 內的 this 指向 state
-      this.allResultdata = {} as IPhotoSearchResult;
       this.showPhotos = [] as Array<ISinglePhoto>;
     },
     /**
      * 更新要顯示的圖片
      */
-    updateShowPhotosData(photos: Array<ISinglePhoto>) {
-      // 如果沒有下一頁的話
-      // 判斷 load 回來的資料是否有圖片可供顯示
-      // this.allResultdata = photoSearchRes;
-      // if (photoSearchRes.hasOwnProperty("photos")) {
-      //   this.showPhotos = this.showPhotos.concat(photoSearchRes.photos);
-      // }
+    updateShowingPhotosData(photos: Array<ISinglePhoto>) {
+      this.showPhotos.push(...photos);
     },
     /**
-     * 更新當前搜尋關鍵字
-     * 但這個好像也不一定需要，可以再想想看
-     * 或者是說，如果 load 回來的資料有至少一張圖片可以顯示的話，那就再把 keyword 存起來
+     * 是最後一頁
      */
-    updateSearchKeyword(keyword: string) {
-      this.searchKeyword = keyword;
-    },
-    /**
-     * 判斷 load 回來的資料
-     * TODO: 待完善
-     */
-    judgeSearchResult(photoSearchRes: IPhotoSearchResult) {
-      console.log("judgeSearchResult", photoSearchRes);
-      if (photoSearchRes.prev_page) {
-        this.havePrevPage = true;
-      }
-      if (photoSearchRes.next_page) {
-        this.haveNextPage = true;
+    isLastPage(photoSearchRes: IPhotoSearchResult) {
+      if (photoSearchRes.prev_page && photoSearchRes.next_page === undefined) {
+        return true;
       }
     },
   },
--- src/views/SearchView.vue
+++ src/views/SearchView.vue
@@ -17,13 +17,13 @@ let loadingPhotosStatus = false;
 // 每頁幾筆
 let perPage = 10;
 // 第幾頁
-let page = 2;
+let page = 1;
 
 console.warn("route.query.q", route.query.q);
 searchKeyword.value = route.query.q as string;
 
 /**
- * 判斷是否有輸入搜尋關鍵字
+ * 判斷網址內是否有搜尋關鍵字
  */
 function haveSearchKeyword() {
   if (searchKeyword.value === "" || searchKeyword.value === undefined) {
@@ -32,42 +32,87 @@ function haveSearchKeyword() {
   return true;
 }
 
-loadPhotos();
 /**
- *
+ * 封裝圖片請求
  */
 function loadPhotos() {
   const searchURL = `https://api.pexels.com/v1/search?query=${searchKeyword.value}&per_page=${perPage}&page=${page}`;
-
+  // const searchURL = `https://api.pexels.com/v1/search?query=cube&per_page=${perPage}&page=165`; // test for judge last page function
   if (loadingPhotosStatus) {
-    return;
+    throw new Error("目前仍在請求圖片中，請勿重複請求");
   }
+  return axios.get(searchURL, {
+    headers: {
+      Authorization: import.meta.env.VITE_API_AUTH,
+    },
+  });
+}
 
-  axios
-    .get(searchURL, {
-      headers: {
-        Authorization: import.meta.env.VITE_API_AUTH,
-      },
-    })
-    .then((res) => {
-      console.warn("res", res);
-      // 將資料存入 Pinia 中
-      searchResult.updatePhotosData(res.data);
-    })
-    .catch((err) => {})
-    .finally(() => {});
+/**
+ * 載入更多圖片
+ */
+async function loadMorePhotos() {
+  try {
+    page++;
+    const res = await loadPhotos();
+    // console.log("initial res", res);
+    // 判斷搜尋結果(是否有上下一頁、目前是否有圖片)
+    searchResult.judgeSearchResult(res.data);
+    // 更新圖片顯示陣列
+    searchResult.updateShowingPhotosData(res.data.photos);
+  } catch (err) {
+    // 失敗的話則將剛剛 + 1 的 page 減回來
+    page--;
+    console.error(err);
+  } finally {
+    loadingPhotosStatus = false;
+  }
 }
 
 // 沒有輸入關鍵字
 if (!haveSearchKeyword()) {
+  // 導回首頁
   router.push({
     path: "/",
   });
+}
+// 有輸入關鍵字
+// 使用 Pinia 儲存該關鍵字
+searchResult.updateSearchKeyword(route.query.q);
+
+initial();
+/**
+ * 進入頁面後需要做的事情
+ */
+async function initial() {
+  try {
+    page = 1;
+    const res = await loadPhotos();
+    // console.warn("initial res", res);
+    // 判斷搜尋結果(是否有上下一頁、目前是否有圖片)
+    searchResult.judgeSearchResult(res.data);
+    // 更新圖片顯示陣列
+    searchResult.updateShowingPhotosData(res.data.photos);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    loadingPhotosStatus = false;
+  }
 }
 </script>
 
 <template>
   <div>This is search page.</div>
+  <p>Search Keyword is: {{ searchResult.searchKeyword }}</p>
+  <p>目前 load 到第幾頁了： {{ page }}</p>
+  <div v-for="photo in searchResult.showPhotos" :key="photo.id">
+    <!-- portrait 會統一尺寸，還不錯，就用這個吧 -->
+    <img :src="photo.src.portrait" />
+  </div>
+  <!-- TODO: 如果沒有更多圖片了，則不顯示此按鈕，並顯示一個區塊提示使用者已經看完所有圖片 -->
+  <button type="button" @click="() => loadMorePhotos()">
+    load more photos
+  </button>
 </template>
 
 <style scoped></style>

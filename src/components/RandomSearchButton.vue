eee49a18d84cddf373bd9d98d66d3d7839e91ff0
Pony Hsieh <lbjwin@gmail.com>
2023-02-14 14:25:34 +0800

    feat: 新增 RandomSearchButton.vue
    隨機搜尋按鈕
    搭配套件 random-words 進行隨機搜尋

---
+++ src/components/RandomSearchButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+import randomWords from "random-words";
+
+const router = useRouter();
+function doRandomSearch() {
+  router.push({
+    path: "/search",
+    query: {
+      q: randomWords(1)[0],
+    },
+  });
+}
+</script>
+
+<template>
+  <button type="button" @click="doRandomSearch">試試好手氣！</button>
+</template>
+
+<style scoped></style>

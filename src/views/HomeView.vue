9f8f9ca2e420315dbbf18009bf46600b4b021a65
Pony Hsieh <lbjwin@gmail.com>
2023-02-14 14:32:00 +0800

    fear: 在 Home 頁面中引入 SearchBar, RandomSearchButton

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SearchBar from "../components/SearchBar.vue";
+import RandomSearchButton from "../components/RandomSearchButton.vue";
+</script>
 
 <template>
   <main>This is Home Page</main>
+  <SearchBar />
+  <RandomSearchButton />
 </template>

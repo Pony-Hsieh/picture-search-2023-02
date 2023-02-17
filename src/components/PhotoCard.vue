a36630728cff7a0f77a068d36b354eb62bf971d2
Pony Hsieh <lbjwin@gmail.com>
2023-02-17 16:25:32 +0800

    feat: 新增 PhotoCard.vue

---
+++ src/components/PhotoCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex justify-center items-center">
+    <img
+      :src="singlePhotoData.src.portrait"
+      :alt="singlePhotoData.alt"
+      class="mw-full mh-full"
+    />
+  </div>
+</template>
+
+<!-- TODO: 嘗試用 <script setup lang="ts"></script> 改寫看看-->
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  // 要接收哪些傳入的屬性(props)
+  props: ["singlePhotoData"],
+});
+</script>

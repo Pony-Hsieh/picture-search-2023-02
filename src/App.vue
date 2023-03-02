06751d69e2e720438968d2aa5d57dfcc766d7818
Pony Hsieh <lbjwin@gmail.com>
2023-03-02 10:21:38 +0800

    feat: 新增 transition 效果

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,29 @@ import { RouterView } from "vue-router";
 </script>
 
 <template>
-  <RouterView />
+  <RouterView v-slot="{ Component, route }">
+    <transition name="fade" mode="out-in">
+      <div :key="route.name">
+        <component :is="Component" />
+      </div>
+    </transition>
+  </RouterView>
 </template>
 
-<style scoped></style>
+<style scoped>
+/* <transition name="fade"> */
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-to,
+.fade-leave-from {
+  opacity: 1;
+}
+.fade-enter-active {
+  transition: opacity 1s;
+}
+.fade-leave-active {
+  transition: opacity 0.8s;
+}
+</style>
